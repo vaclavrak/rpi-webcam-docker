d871fca3a03267b38aa68b20b4b8f98ecdb83e40
Vaclav Rak <vaclavrak@users.noreply.github.com>
2016-11-25 07:58:20 +0100

    removed -f for tag

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ tag=$1
 
 fi
 
-docker tag -f webcam hub.srv.webeye.services:5000/webeye/webcam
-docker tag -f webcam hub.srv.webeye.services:5000/webeye/webcam:$tag
+docker tag webcam hub.srv.webeye.services:5000/webeye/webcam
+docker tag webcam hub.srv.webeye.services:5000/webeye/webcam:$tag
 
 
 if [[ $tag != 'dev' ]]; then
@@ -25,26 +25,26 @@ fi
 
 
 docker build -t beat beat/
-docker tag -f beat hub.srv.webeye.services:5000/webeye/webcam-beat
+docker tag beat hub.srv.webeye.services:5000/webeye/webcam-beat
 if [[ $tag != 'dev' ]]; then
- docker tag -f beat hub.srv.webeye.services:5000/webeye/webcam-beat:$tag
+ docker tag beat hub.srv.webeye.services:5000/webeye/webcam-beat:$tag
  docker push hub.srv.webeye.services:5000/webeye/webcam-beat
  docker push hub.srv.webeye.services:5000/webeye/webcam-beat:$tag
 fi
 
 
 docker build -t work work/
-docker tag -f work hub.srv.webeye.services:5000/webeye/webcam-worker
+docker tag work hub.srv.webeye.services:5000/webeye/webcam-worker
 if [[ $tag != 'dev' ]]; then
- docker tag -f work hub.srv.webeye.services:5000/webeye/webcam-worker:$tag
- docker push hub.srv.webeye.services:5000/webeye/webcam-worker
+ docker tag work hub.srv.webeye.services:5000/webeye/webcam-worker:$tag
+ docker push hub.removed srv.webeye.services:5000/webeye/webcam-worker
  docker push hub.srv.webeye.services:5000/webeye/webcam-worker:$tag
 fi
 
 docker build -t serial serial/
-docker tag -f serial hub.srv.webeye.services:5000/webeye/webcam-serial
+docker tag serial hub.srv.webeye.services:5000/webeye/webcam-serial
 if [[ $tag != 'dev' ]]; then
- docker tag -f serial hub.srv.webeye.services:5000/webeye/webcam-serial:$tag
+ docker tag serial hub.srv.webeye.services:5000/webeye/webcam-serial:$tag
  docker push hub.srv.webeye.services:5000/webeye/webcam-serial
  docker push hub.srv.webeye.services:5000/webeye/webcam-serial:$tag
 fi
